578460de7502c3fa212ade35a740fd0b870a8fa1
Hung Dung <hungdung.nnl@gmail.com>
2024-08-14 21:35:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 window.onload=function(){
-  //  Swiper research
-  var swiper = new Swiper(".right-container", {
-    grabCursor: 'true',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
   // Swiper news
-  var swiper = new Swiper(".news-container", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+  var swiper = new Swiper(".button-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    grabCursor: 'true',
+    
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".news-container .swiper-button-next",
+      prevEl: ".news-container .swiper-button-prev",
+    },
+    breakpoints:{
+      0:{
+        slidesPerView: 1,
+      },
+      768:{
+        slidesPerView: 2,
+      },
+      950:{
+        slidesPerView: 3,
+      },
+    }
+  });
+
+
+
+  //  Swiper research
+  var swiper = new Swiper(".container-swiper", {
+    grabCursor: 'true',
+    spaceBetween: 1,
+    navigation: {
+      nextEl: ".right-container .swiper-button-next",
+      prevEl: ".right-container .swiper-button-prev",
     },
   });
-}
